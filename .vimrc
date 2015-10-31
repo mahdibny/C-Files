17a5f1dd239df3049191d9b520c22fd6ce24d23d
Mahdi <mahdibny@gmail.com>
2015-10-31 06:59:49 -0400

    add .vimrc

--- .vimrc
+++ .vimrc
@@ -1,6 +1,8 @@
 syntax on
 filetype plugin indent on
-:imap jj <Esc>
+:imap jk <Esc>
+:imap JK <Esc>
 set tabstop=4
 set shiftwidth=4
 set number
+set clipboard=unnamed
--- ex8.c
+++ ex8.c
@@ -6,9 +6,20 @@ int main (int argc, char* argv[]){
 	char full[]={'m','a','h','d','i',' ','m','.',' ' ,'h','o','s','s','a','i','n','\0'};
 	
 	printf("The size of an int: %ld\n", sizeof(int));
+	// the size of an int here is equal to 4 bytes and it depends
+	// on the version of your computer such as 32 and 64 bit
+	
 	printf("The size of areas (int[]): %ld\n", sizeof(areas));
+	// so here we look at the size of the array and the computer 
+	// gives us 20 which makes sense, we have 5 integers each with
+	// 4 bytes of space that they take up
+	
 	printf("The number of ints in areas: %ld\n",sizeof(areas)/sizeof(int));
+	// then when we divide the size of the array by the type of the said
+	// array we obtain the number of elements in the array 
+	
 	printf("The first area is %d, the 2nd %d.\n",areas[0],areas[1]);
+
 	printf("The size of a char: %ld\n", sizeof(char));
 	printf("The size of a name (char[]): %ld\n", sizeof(name));
 	printf("The size of chars: %ld\n", sizeof(name)/sizeof(char));
